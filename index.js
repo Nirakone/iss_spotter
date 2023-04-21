fbbb00b7d7ee46423555592ee333f61646fa7a6f
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-04-21 04:33:41 -0400

    updates with added functions

--- index.js
+++ index.js
@@ -9,3 +9,46 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('45.74.111.172', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '42.958684575522305', longitude: '-81.20360234858241' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  
+  printPassTimes(passTimes);
+});
